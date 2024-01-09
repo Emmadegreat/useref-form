99b1dabfc42aad0b420cfee44e0b744db64fe67d
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-01-09 21:42:38 +0100

    add

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -26,7 +26,7 @@ const Form = () => {
     }
     return (
         <div className='flex justify-center items-center m-auto flex-col pt-24'>
-            <h1 className='md:text-4xl text-2xl text-blue-800 md:font-bold font-semibold'>React Form using UseRef Hook</h1>
+            <h1 className='md:text-3xl text-2xl text-blue-800 md:font-bold font-semibold'>React Form using UseRef Hook</h1>
 
             <form action="" onSubmit={handleSubmit}
                 className='w-[70%] md:w-[400px] py-4 border-2 rounded p-4 my-4 border-blue-800'>
